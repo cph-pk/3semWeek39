a3ea6c12b3de169fb4856198fce0cb95f2258d79
Per <cph-pk171@cphbusiness.dk>
2020-09-25 09:42:25 +0200

    first commit

---
+++ Wedensday, Thursday and Friday/Flow2Week1Day3/src/test/java/facades/PersonFacadeTest.java
@@ -0,0 +1,79 @@
+package facades;
+
+import dto.PersonDTO;
+import dto.PersonsDTO;
+import entities.Person;
+import java.util.List;
+import utils.EMF_Creator;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+//Uncomment the line below, to temporarily disable this test
+//@Disabled
+public class PersonFacadeTest {
+
+    private static EntityManagerFactory emf;
+    private static PersonFacade facade;
+
+    public PersonFacadeTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactoryForTest();
+        facade = PersonFacade.getPersonFacde(emf);
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+//        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
+    }
+
+    // Setup the DataBase in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the script below to use YOUR OWN entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("Person.deleteAllRows").executeUpdate();
+            em.persist(new Person("Kurt", "Wonnegut", "1234"));
+            em.persist(new Person("Jønke", "bbb", "6666"));
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+//        Remove any data after each test was run
+    }
+
+    // TODO: Delete or change this method 
+    @Test
+    public void testAFacadeMethod() {
+        assertEquals(2, facade.getPersonFacadeCount(), "Expects two rows in the database");
+    }
+
+
+    @Test
+    public void testGetPersonFacadeCount() {
+        assertEquals(2,facade.getPersonFacadeCount(),"Expects two persons in the database");
+    }
+    
+//    @Test
+//    public void testGetAllPersons() {
+//        PersonsDTO p = facade.getAllPersons();
+//        assertThat(p, hasSize(2));
+//    }
+}
